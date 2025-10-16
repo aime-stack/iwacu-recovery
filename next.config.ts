afe2a7a42b05c617d4ab61a088022f2fb9329298
codeWithAime <mwizerwaaime9@gmail.com>
2025-10-16 20:11:26 +0200

    Added SEO

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "iwacurecoverycentre.com", // non-www domain
+          },
+        ],
+        destination: "https://www.iwacurecoverycentre.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
